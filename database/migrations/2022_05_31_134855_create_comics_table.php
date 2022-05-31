2f1d1fea249f2915a4838b24540bd1021bc08982
NuvoloniM <nuvoloni.m@gmail.com>
2022-05-31 16:03:11 +0200

    compilazione migration

---
+++ database/migrations/2022_05_31_134855_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('thumb', 100)->nullable();
+            $table->decimal('price', 5, 2);
+            $table->date('sale_date')->default(new DateTime());
+            $table->enum('type', ['comic book', 'graphic novel']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
