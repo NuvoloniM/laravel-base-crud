603e2367f143f441f39585f3a151fa8bce91d7c4
NuvoloniM <nuvoloni.m@gmail.com>
2022-06-01 17:09:19 +0200

    creazione nuovo js per  delete +webpack + run dev

---
+++ resources/js/deleteMessage.js
@@ -0,0 +1,3 @@
+// creo la logica del delete in un file a parte, diverso da app.js. dovrò lanciare di nuovo run dev e run watch per collegare e far creare in public la cartella 
+// prima però devo aggiungere il percorso in webpack
+
